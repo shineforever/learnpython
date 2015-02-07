dcd9dae743e51c1cab3520d9be686a661d5feafb
Q1mi <liwenzhou7@gmail.com>
2015-02-07 14:31:15 +0800

    add comments

--- 20150205/login_3.py
+++ 20150205/login_3.py
@@ -12,30 +12,27 @@ while True:
 	account_list = f.readlines()
 	f.close() 
 	loginSuccess = False
-	username = raw_input('username:').strip()
+	username = raw_input('username:').strip()#remove the space
 	lock_list = []
-#	lock_file = 'lock.txt'
 	f = file(lock_file)
 	for i in f.readlines():
-		h = i.strip('\n')
+		h = i.strip('\n')#remove the line break
 		lock_list.append(h)
 	f.close()
 #	print lock_list
 	if username in lock_list:
 		print 'Sorry!%s has been locked!' % username
 		sys.exit()
-
-
 	for line in account_list:
-		line = line.split()
+		line = line.split()#每一行以空格分开为line[0]和line[1]
 		if username == line[0]:
 			for i in range(3):
 				password = raw_input('password:').strip()
-				if password == line[1]:
+				if password == line[1]:#password success
 					print 'Welcome %s login my system!' % username
 					loginSuccess = True
 					break
-			else:
+			else:#三次输错，将用户名写入lock.txt
 				f = file(lock_file,'a')
 				f.write('%s\n' % username)
 				f.close()
