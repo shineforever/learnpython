0e18dd4dff013e1857797e6f34600685ce67fd45
Q1mi <liwenzhou7@gmail.com>
2015-12-22 16:57:25 +0800

    test!

---
+++ 20151221/test00.py
@@ -0,0 +1,11 @@
+lock_file = "lock.txt"
+account_file = "account.txt"
+
+
+with open(account_file) as f:
+	account_list = f.readlines()
+for i in account_list:
+	print(i)
+	h = i.split()
+	print(h[0])
+	print(h[1])
