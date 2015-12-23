3e4f4698b6d28aeb82aeac93a1af7e71bd79e2c4
Q1mi <liwenzhou7@gmail.com>
2015-12-23 17:02:03 +0800

    day2,not ok!

--- 20151221/homework01.py
+++ 20151221/homework01.py
@@ -7,39 +7,45 @@
 -认证成功后显示欢迎信息
 -输错三次后锁定
 '''
-import os
+
 import sys
+import getpass
 
 account_file = "account.txt"
 lock_file = "lock.txt"
+lock_list = []
 isLocked = False
+loginSuccess = False
 
 user_name = input("please input your user_name:").strip()	#去掉空格
-with open(account_file) as f1:
-	account_list = f1.readlines()
-	for i in ff.readlines():
+with open(lock_file) as f1:
+	for i in f1.readlines():
 		i = i.strip()
-		account_list.append(i)
+		lock_list.append(i)
 if user_name in lock_list:
 	print("Sorry!%s were locked!"% user_name)
 	isLocked = True
-	sys.exit(0)
 while not isLocked:
-	with open(account_file) as f:
-		account_list = f.readlines()
+	with open(account_file) as f2:
+		account_list = f2.readlines()
 	for line in account_list:
 		line = line.split()
 		if user_name == line[0]:
 			for i in range(3):
-				pass_word = input("Passwd:")
+				pass_word = input("Password:")
 				if pass_word == line[1]:
 					print("Welcome to login!")
+					loginSuccess = True
+					isLocked = True
 					break
+			if loginSuccess is True:break
 			else:
-				with open(lock_file, 'a') as f:
+				with open(lock_file, 'at') as f:
 					f.write('%s\n'% user_name)
-					print("Too many tries!You will be locked!")
-				sys.exit(0)
+				print("Too many tries!You will be locked!")
+				isLocked=True
+				break
 		else:
 			print("You haven't registered!")
+			isLocked=True
 			break
