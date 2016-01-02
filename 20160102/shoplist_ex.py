d2c691f7e064233e0b082737f8a3b1862ad98856
Q1mi <liwenzhou7@gmail.com>
2016-01-02 14:45:02 +0800

    add a exercise about shopping list

---
+++ 20160102/shoplist_ex.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+
+"""
+	题目：购物小程序
+	需求：程序启动后，要求用户输入购物预算，然后打印购物菜单，菜单格式如下：
+	Welcome to Alex's shopping mall,below are the things we are selling:
+		1. MacBook Air       7999
+		2. Starbucks Coffee  33
+		3. iphone 6 Plus     6188
+		4. ...
+	用户可以不断的购买商品，程序要实时的把购买的商品添加到购物车，并且从预算金额中扣掉相应商品的价格，
+	可购买的商品总值不能超过预算总值，用户选择退出后，打印他已购商品及所剩金额。
+"""
+
