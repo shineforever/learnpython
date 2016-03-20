d5582d5a7b090bdbcc4ee19902b3179bab27836a
Q1mi <liwenzhou7@gmail.com>
2016-03-20 21:32:31 +0800

    add a random exercise.

--- test/dnspython/simple1.py
+++ test/dnspython/simple1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import dns.resolver
-
-domain=raw_input('Please input an domain :')	#输入域名地址
-A = dns.resolver.query(domain ,'A')	#指定查询类型为A记录
-
-for i in A.response.answer:	#通过response.answer方法获取查询回应信息
-    for j in i.items:	#遍历回应信息
-        print j.address
+# import dns.resolver
+#
+# domain=raw_input('Please input an domain :')	#输入域名地址
+# A = dns.resolver.query(domain ,'A')	#指定查询类型为A记录
+#
+# for i in A.response.answer:	#通过response.answer方法获取查询回应信息
+#     for j in i.items:	#遍历回应信息
+#         print j.address
