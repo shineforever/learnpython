dc01da188fe24c45da61279ad40e2c8320291a43
Q1mi <liwenzhou7@gmail.com>
2016-04-25 17:10:15 +0800

    add exercise about itertools module.

---
+++ itertools_ex/itertools_permatutions_combinations.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Email: master@liwenzhou.com
+
+"""
+itertools.permutations:排列
+itertools.combinations:组合，需指定元素数
+itertools.permutations_with_replacement:允许相同元素多次出现的组合
+"""
+
+from itertools import permutations
+from itertools import combinations
+from itertools import combinations_with_replacement
+
+
+l1 = [1, 2, 3, 4, 5]
+
+# # 打印所有可能的排列
+# for i_p in permutations(l1):
+# 	print(i_p)
+#
+# # 打印所有可能3位排列
+# for i_p2 in permutations(l1, 3):
+# 	print(i_p2)
+
+# # 打印所有可能的组合,不考虑元素间的实际顺序，已选过的元素会被从候选元素中删除
+# for i_c in combinations(l1, 5):
+# 	print(i_c)
+#
+# # 打印所有可能的3位组合
+# for i_c2 in combinations(l1, 3):
+# 	print(i_c2)
+
+
+# # 已选元素不会被从候选元素中删除
+# for i_cr in combinations_with_replacement(l1, 5):
+# 	print(i_cr)
+#
+# # 已选元素不会被从候选元素中删除
+# for i_cr2 in combinations_with_replacement(l1, 3):
+# 	print(i_cr2)
+
+
