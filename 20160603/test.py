d24f6e80949183ae34d0cb43b31e1758b81ae8c5
Q1mi <liwenzhou7@gmail.com>
2016-06-03 23:07:34 +0800

    add a exercise about how to get three elements that their sum most close to target num

--- 20160603/test.py
+++ 20160603/test.py
@@ -47,34 +47,31 @@ s = 123
 
 
 def find_three(lst, goal):
-	closed = (float("inf"), )
-	closed_value = float("inf")
+	closed = (float("inf"), )  # 定义一个无穷大的元祖，存放要得到的三个数
+	closed_value = float("inf")  # 定义一个用于存储任意三个数的和与目标数的差值（绝对值）
 	for i in range(len(lst)-2):
 		j = i+1
 		k = len(lst)-1
 		while j < k:
-			tmp = tuple(lst[x] for x in (i, j, k))
-			value_tmp = abs(sum(tmp) - goal)
-			if value_tmp == 0:
+			tmp = tuple(lst[x] for x in (i, j, k))  # 得到三个元素组成的元祖
+			value_tmp = abs(sum(tmp) - goal)  # 得到三元素的和与目标数的差值的绝对值
+			if value_tmp == 0:  # 如果三个元素的和等于目标数，返回该三个元素
 				return tmp
 			else:
-				if value_tmp < closed_value:
-					closed_value = value_tmp
-					closed = tmp
-					if sum(tmp) - goal > 0:
-						k -= 1
-					else:
-						j += 1
-				else:
-					break
+				if value_tmp < closed_value:  # 如果差值小于上一个差值
+					closed_value = value_tmp  # 保存这一次的差值
+					closed = tmp  # 保存当前的三个元素
+				if sum(tmp) - goal > 0:  # 如果三元素的和比目标数大，
+					k -= 1  # K往前取值（即要往小的找）
+				j += 1  # 如果三元素的和比目标数小，j往后取（即要找个大的值）
 
 		return closed
 
 
 def main():
-	ll = [1, 3, 5, 7, 9]
-	ss = 17
-	ret = find_three(ll, ss)
+	# ll = [1, 3, 5, 7, 9]
+	# ss = 17
+	ret = find_three(l1, s)
 	print(ret)
 
 if __name__ == "__main__":
